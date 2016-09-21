49e5111999a5266af71563f40f0f29d82ef2f780
Bruce McCulloch <mail@bluenovaconsulting.com>
2016-09-21 10:33:28 -0400

    Update bnchelper.php

--- bnc/bnchelper.php
+++ bnc/bnchelper.php
@@ -6,7 +6,7 @@
 			$plugin_folder = WP_PLUGIN_URL . '/BNC/';    
 			return $plugin_folder;
 		}
-		
+		 
 		 // returns the html string needed for the support email address with proper subject
 		public function get_helpdesk_email_link()
 		{
